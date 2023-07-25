f6fa73d1cfe76ea03f075f366ba145275d47433c
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-07-25 14:53:09 +0200

    Виправив

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,26 +5,18 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const {SECRET_KEY} = process.env;
 
+
 const register = async(req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({email})
-
     if(user) {
         throw HttpError(409, "Email already exists / Такий емейл уже існує")
     } 
-
     const hashPassword = await bcrypt.hash(password, 10)
-
     const newUser = await User.create({...req.body, password: hashPassword});
-
-    // const token = jwt.sign({ id: newUser._id }, SECRET_KEY, { expiresIn: "23h" });
-
     res.status(201).json({
-        // token, 
-        // user: {
-            email: newUser.email,
-            name: newUser.name,
-        // }
+        email: newUser.email,
+        name: newUser.name,
     })
 }
 
